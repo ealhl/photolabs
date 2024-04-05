fb8cec8a0e0c652dbcf38d1e5b29d8df124a1853
ealhl <yayt.tuk@gmail.com>
2024-04-05 16:23:15 -0700

    fix photo list item css style

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,12 +5,12 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   /* Insert React */
   return (
-    <div className="photo-list__item">
-<img className="photo-list-item__image" src={props.photo.imageSource} alt="Photo" />
-<img className="photo-list-item__profile" src={props.photo.profile} alt="Profile" />
-<div className="photo-list-item__username">{props.photo.username}</div>
-<div className="photo-list-item__location">{props.photo.location.city}, {props.photo.location.country}</div>
-    </div>
+<div className="photo-list__item">
+  <img className="photo-list__item-image" src={props.photo.imageSource} alt="Photo" />
+  <img className="photo-list__item-profile" src={props.photo.profile} alt="Profile" />
+  <div className="photo-list__item-username">{props.photo.username}</div>
+  <div className="photo-list__item-location">{props.photo.location.city}, {props.photo.location.country}</div>
+</div>
   );
 };
 
