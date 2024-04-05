287f14402f32f41589ee5a15e8a093a0af2674a8
ealhl <yayt.tuk@gmail.com>
2024-04-05 00:28:15 -0700

    add photolist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoList from './components/PhotoList';
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 
 // Note: Rendering a single component to build components in isolation
+//Refactor PhotoListItem to use props. Then pass in the sampleDataForPhotoListItem via props.
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem {...sampleDataForPhotoListItem} />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,29 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return (
+    <div className="photo-list-item">
+      <img
+        className="photo-list-item__image"
+        src={props.imageSource}
+        alt="Photo"
+      />
+      <img
+        className="photo-list-item__profile"
+        src={props.profile}
+        alt="Profile"
+      />
+        <div className="photo-list-item__username">
+          {props.username}
+        </div>
+      <div className="photo-list-item__location">
+        {props.location.city},{" "}
+        {props.location.country}
+      </div> 
+    </div>
+  );
 };
 
 export default PhotoListItem;
