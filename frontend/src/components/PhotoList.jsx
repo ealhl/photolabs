70cfd4a649e27f1a78e9b564841a6e02901e2f2d
ealhl <yayt.tuk@gmail.com>
2024-04-07 08:56:58 -0700

    add modal fav list

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
-const PhotoList = ({ photos, favouritePhotos, toggleFavouritePhoto, openModal }) => {
+const PhotoList = ({ photos, favouritePhotos = [], toggleFavouritePhoto, openModal }) => {
   const handleFavouriteToggle = (photoId) => {
     toggleFavouritePhoto(photoId);
   };
+
   return (
     <ul className="photo-list">
-      {photos.map((photo) => (
+      {photos.map((photo) => ( 
         <PhotoListItem
           key={photo.id}
           photo={photo}
