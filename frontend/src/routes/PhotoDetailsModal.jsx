1f3cd683a1722bd95437576bc4188a20fedac91f
ealhl <yayt.tuk@gmail.com>
2024-04-07 09:02:30 -0700

    fix some csss

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,53 +1,35 @@
-import React from "react";
-import { useEffect } from "react";
-import "../styles/PhotoDetailsModal.scss";
-import closeSymbol from "../assets/closeSymbol.svg";
+import React from 'react';
+import { useEffect } from 'react';
+import '../styles/PhotoDetailsModal.scss'
+import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from "components/PhotoList";
 
 const PhotoDetailsModal = ({ photo, closeModal }) => {
   useEffect(() => {
-    console.log("PhotoDetailsModal", photo); // Log the selected photo data
-  }, [photo]);
-
+  }, [photo]); 
+  
   const handleCloseClick = () => {
     closeModal(); // Call closeModal function when the close button is clicked
   };
   const similarPhotosArray = Object.values(photo.similar_photos || {});
   return (
     <div className="photo-details-modal">
-      <button
-        className="photo-details-modal__close-button"
-        onClick={handleCloseClick}
-      >
+      <button className="photo-details-modal__close-button" onClick={handleCloseClick}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <div className="photo-details-modal__header">
-        <img
-          className="photo-details-modal__images"
-          src={photo.urls.full}
-          alt="Selected Photo"
-        />
-        <div className="photo-list__user_info">
-          <img
-            className="photo-list__user-profile"
-            src={photo.user.profile}
-            alt="Profile"
-          />
-          <div>
-            <div className="photo-list__user-details">
-              {photo.user.username}
-            </div>
-            <div className="photo-list__user-location">
-              {photo.location.city}, {photo.location.country}
-            </div>
-          </div>
-        </div>
-      </div>
+      <img className="photo-details-modal__image" src={photo.urls.full} alt="Selected Photo" />
+    <img className="photo-list__user-profile" src={photo.user.profile} alt="Profile" />
+    <div>
+      <div className="photo-list__user-details">{photo.user.username}</div>
+     <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
+    </div>
+  </div>
       <div className="photo-details-modal__images">
-        <PhotoList photos={similarPhotosArray} openModal={null} />
+          <PhotoList photos={similarPhotosArray}/>
       </div>
     </div>
-  );
+  )
 };
 
 export default PhotoDetailsModal;
